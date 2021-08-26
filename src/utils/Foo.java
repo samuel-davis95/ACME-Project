1c83154044bbcb657bd5206f4586820f6beb3887
samuel-davis95 <samadavis123@gmail.com>
2021-08-26 10:01:42 -0400

    added a line

---
+++ src/utils/Foo.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		System.out.println("howdy");
+		System.out.println("hi from GitHub");
+		System.out.println("hi 'dave1' branch, from Eclipse");
+		System.out.println("hello worlds");
+		System.out.println("adding a line");
+
+	}
+
+}
